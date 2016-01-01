47f8e7b196f59d730a0b1f00846cf7cbee8a3b90
Sarun Wongpatcharapakorn <artwork.th@gmail.com>
2016-01-01 21:41:17 +0700

    Remove unused code that cause warning
    To make pod lint pass.

--- Pod/Classes/SWImagePickerManager.swift
+++ Pod/Classes/SWImagePickerManager.swift
@@ -36,10 +36,6 @@ public class SWImagePickerManager: NSObject, UIImagePickerControllerDelegate, UI
             self.showImagePickerWithSourceType(.PhotoLibrary, fromViewController: viewController)
         }
         
-        let lastPhoto = UIAlertAction(title: "Last Photo Taken", style: .Default) { (action) -> Void in
-            
-        }
-        
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
             actionSheet.addAction(photoLibrary)
         }
