998ad4fc93bad02f3cbcae13f473e0a6e5cef38c
Sarun Wongpatcharapakorn <artwork.th@gmail.com>
2016-02-27 12:38:04 +0700

    Remove ignored files

--- Example/Pods/Target Support Files/Pods-SWImagePickerManager_Tests/Pods-SWImagePickerManager_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_SWImagePickerManager_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SWImagePickerManager_TestsVersionString[];
-
--- Example/Pods/Target Support Files/SWImagePickerManager/SWImagePickerManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SWImagePickerManager : NSObject
-@end
-@implementation PodsDummy_SWImagePickerManager
-@end
